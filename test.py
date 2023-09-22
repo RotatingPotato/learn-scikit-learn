dfc6927e4a72bcc950670a90742f47a8a193dbce
KageRyo <k6.developer@gmail.com>
2023-09-22 14:46:19 +0800

    add: print information.

--- test.py
+++ test.py
@@ -9,11 +9,11 @@ from sklearn.linear_model import LinearRegression
 
 # 產生資料
 rng = np.random.RandomState(42)
-x = 50 * rng.rand(50)
-y = 2 * x - 1 + rng.randn(50)
+x = 50 * rng.rand(1000)
+y = 2 * x - 1 + rng.randn(1000)
 
 # 建立模型
-model=LinearRegression(fit_intercept=True)
+model = LinearRegression(fit_intercept=True)
 
 # 訓練模型
 X = x[:, np.newaxis]
@@ -24,6 +24,10 @@ model.fit(X, y)
 xfit = np.linspace(-1, 50)
 Xfit = xfit[:, np.newaxis]
 yfit = model.predict(Xfit)
+
+# 印出結果
+print("模型參數：\n", model.coef_, model.intercept_)
+print("\n預測結果：\n", yfit)
 
 # 繪圖
 plt.scatter(x, y)
